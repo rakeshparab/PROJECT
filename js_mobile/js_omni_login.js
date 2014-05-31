b5de123b7693f0ad4262033f82a3c1dcae780919
rakeshparab <rakeshparabdir@gmail.com>
2014-05-31 13:17:34 +0530

    Create js_omni_login.js

---
+++ js_mobile/js_omni_login.js
@@ -0,0 +1,19 @@
+function loginbut()
+{
+	
+	alert('123');
+var form = $("#HLogin");
+var u = $("#Lusername", form).val();
+
+var p = $("#Lpassword", form).val();
+	
+if(u != '' && p!= '') 
+{
+
+window.open('index_scan.html');
+
+}
+
+	
+	}
+	
