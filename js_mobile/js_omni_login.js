81e2c61d31a4a4ef0e37df9ee6679581282673bc
rakeshparab <rakeshparabdir@gmail.com>
2014-06-03 11:44:05 +0530

    Update js_omni_login.js

--- js_mobile/js_omni_login.js
+++ js_mobile/js_omni_login.js
@@ -10,7 +10,11 @@ var p = $("#Lpassword", form).val();
 if(u != '' && p!= '') 
 {
 
-window.open('index_scan.html');
+//window.open('index_scan.html');
+              
+day = new Date();
+id = day.getTime();
+eval("page" + id + " = window.open('index_scan.html', '" + id + "', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=1366,height=768');");
 
 }
 
